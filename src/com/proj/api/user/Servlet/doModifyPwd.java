1fa5d2e22dac859745b3715932c60ab3348a8e76
laujangit@gmail.com <1urmygithub>
2017-11-15 13:11:53 +0800

    add interface

---
+++ src/com/proj/api/user/Servlet/doModifyPwd.java
@@ -0,0 +1,21 @@
+package com.proj.api.user.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by jangitlau on 2017/11/15.
+ */
+public class doModifyPwd extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
