91bdbc256dd8a024e3d1df973327bc6701fb3a9f
laujangit@gmail.com <1urmygithub>
2017-11-15 14:50:49 +0800

    add interface

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -15,21 +15,21 @@ import com.proj.api.user.controller.PreAuthorization;
  */
 public class Main {
     public static void main(String[] args){
-        try {
-            PreAuthorization bb = new PreAuthorization("test");
-
-//            new Authorization("test", bb.rand(), "098f6bcd4621d373cade4e832627b4f6");
-        } catch (UsernameNotExistException e) {
-            e.printStackTrace();
-        } catch (RelationalDatabaseException e) {
-            e.printStackTrace();
-        } catch (NonRelationalDatabaseException e) {
-            e.printStackTrace();
-        } catch (AESEncryptException e) {
-            e.printStackTrace();
-        } catch (MalformedJsonException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
+//        try {
+//            PreAuthorization bb = new PreAuthorization("test");
+//
+////            new Authorization("test", bb.rand(), "098f6bcd4621d373cade4e832627b4f6");
+//        } catch (UsernameNotExistException e) {
+//            e.printStackTrace();
+//        } catch (RelationalDatabaseException e) {
+//            e.printStackTrace();
+//        } catch (NonRelationalDatabaseException e) {
+//            e.printStackTrace();
+//        } catch (AESEncryptException e) {
+//            e.printStackTrace();
+//        } catch (MalformedJsonException e) {
+//            e.printStackTrace();
+//        }
+//        System.exit(0);
     }
 }
