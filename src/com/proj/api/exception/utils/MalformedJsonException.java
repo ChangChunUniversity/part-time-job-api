588ed636e771f327316e3fce62c2e1e4b21659db
laujangit@gmail.com <1urmygithub>
2017-11-15 16:02:35 +0800

    add interface

--- src/com/proj/api/exception/utils/MalformedJsonException.java
+++ src/com/proj/api/exception/utils/MalformedJsonException.java
@@ -6,7 +6,8 @@ import com.proj.api.exception.error.CustomError;
  * Created by jangitlau on 2017/11/15.
  */
 public class MalformedJsonException extends CustomError{
+    private static final int err_code = 405;
     public MalformedJsonException() {
-        super(411);
+        super(err_code);
     }
 }
