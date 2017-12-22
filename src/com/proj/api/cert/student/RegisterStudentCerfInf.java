8192d1bb2abb57b1ef622076ee35fe0be5609051
laujangit@gmail.com <1urmygithub>
2017-12-22 17:15:15 +0800

    Product of HaoZhihong

--- src/com/proj/api/cert/student/RegisterStudentCerfInf.java
+++ src/com/proj/api/cert/student/RegisterStudentCerfInf.java
@@ -8,6 +8,7 @@ import com.proj.api.exception.auth.UserNotAuthorizedException;
 import com.proj.api.exception.cert.InvalidCertTypeException;
 import com.proj.api.exception.database.NonRelationalDatabaseException;
 import com.proj.api.exception.database.RelationalDatabaseException;
+import com.proj.api.exception.other.InvalidCheckCodeException;
 import com.proj.api.exception.utils.MalformedJsonException;
 import com.proj.api.utils.AuthorizationUtils;
 
@@ -18,7 +19,27 @@ import java.sql.SQLException;
  * Created by jangitlau on 2017/12/12.
  */
 public class RegisterStudentCerfInf {
-    public RegisterStudentCerfInf(String login_id, int cert_status, String co_name, String co_nickname, int industry_type, int co_type, String org_code, String biz_code, String org_pic, String biz_pic, int co_scale, String co_desc, String contact_person, String contact_phone, String contact_mail, String co_site, String[] co_pic_array, int co_city, String co_addr, String check_code) throws UserNotAuthorizedException, UserDisableException, NonRelationalDatabaseException, MalformedJsonException, InvalidOperationException, InvalidBackstageOperationException, RelationalDatabaseException, InvalidCertTypeException {
-        
+    public RegisterStudentCerfInf(String login_id, String user_id, String stu_name, int stu_school, String stu_id, String stu_pwd, String id_code, String id_front_pic, String id_back_pic, int stu_college, int stu_major, String stu_adms_year, String stu_grad_year, String check_code) throws UserNotAuthorizedException, UserDisableException, NonRelationalDatabaseException, MalformedJsonException, InvalidCheckCodeException, InvalidOperationException, InvalidBackstageOperationException, InvalidCertTypeException, RelationalDatabaseException {
+        AuthorizationUtils authorizationUtils = new AuthorizationUtils(login_id);
+        authorizationUtils.checkParams(id_back_pic + id_code + id_front_pic + login_id + stu_adms_year + stu_college + stu_grad_year + stu_id + stu_major + stu_name + stu_pwd + stu_school + user_id, check_code);
+
+        if (authorizationUtils.getiType() != 2) {
+            throw new InvalidCertTypeException();
+        }
+
+        RelationalDatabase relationalDatabase = new RelationalDatabase();
+        ResultSet resultSet;
+        resultSet = relationalDatabase.doQuery("select uuid from student_inf where uuid=?", new Object[]{user_id});
+        try {
+            if (resultSet.first()) {
+                relationalDatabase.doSQL("delete from student_inf where uuid=?", new Object[]{user_id});
+            }
+        } catch (SQLException e) {
+            relationalDatabase.close();
+            throw new RelationalDatabaseException(e);
+        }
+        relationalDatabase.doSQL("insert into student_inf (id_back_pic,id_code,id_front_pic,login_id,stu_adms_year,stu_college,stu_grad_year,stu_id,stu_major,stu_name,stu_pwd,stu_school,user_id",
+                new Object[]{id_back_pic, id_code, id_front_pic, login_id, stu_adms_year, stu_college, stu_grad_year, stu_id, stu_major, stu_name, stu_pwd, stu_school, user_id});
+        relationalDatabase.close();
     }
 }
